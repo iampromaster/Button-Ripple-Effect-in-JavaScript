d347561f60c6aa116b28f66cd1919e9b7f643d20
pypymi <dr.mizoguchi@gmail.com>
2024-07-26 10:10:45 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector(".btn")
+
+btn.addEventListener("click", function(e){
+  let x = e.clientX - e.target.offsetLeft;
+  let y = e.clientY - e.target.offsetTop;
+
+  let ripple = document.createElement("span");
+  ripple.style.left = x + "px";
+  ripple.style.top = y + "px";
+  this.appendChild(ripple);
+
+  setTimeout(() => {
+    ripple.remove()
+  }, 1000);
+})
